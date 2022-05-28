aa40e3cd2f8e422796aa48dce946752e74dc947a
vazquezjose <60374151+vazquezjose@users.noreply.github.com>
2022-05-28 11:14:08 -0500

    12 de marzo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,25 +8,31 @@ import { IonicModule } from '@ionic/angular';
 import { PerfilComponent } from './perfil/perfil.component';
 import { PublicacionesComponent } from './publicaciones/publicaciones.component';
 
+import { TabsComponent } from './tabs/tabs.component';
+
+import { PublicacionComponent } from './publicacion/publicacion.component';
+
 import { FormsModule } from '@angular/forms';
 import { RoutesModule } from './routes.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FeedComponent,
-    PerfilComponent,
-    PublicacionesComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    IonicModule.forRoot(),
-    RoutesModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent], 
-  exports: [RoutesModule]
+	declarations: [
+		AppComponent,
+		FeedComponent,
+		PerfilComponent,
+		TabsComponent,
+		PublicacionComponent,
+		PublicacionesComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		AppRoutingModule,
+		IonicModule.forRoot(),
+		RoutesModule
+	],
+	providers: [],
+	bootstrap: [AppComponent], 
+	exports: [RoutesModule]
 })
 export class AppModule { }
--- src/app/routes.module.ts
+++ src/app/routes.module.ts
@@ -3,15 +3,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { FeedComponent } from './feed/feed.component';
 import { PerfilComponent } from './perfil/perfil.component';
 
+import { PublicacionComponent } from './publicacion/publicacion.component';
+
 const routes: Routes = [
-  { path: 'feed', component: FeedComponent },
-  { path: 'perfil', component: PerfilComponent }
+	{
+		path: 'feed',
+		component: FeedComponent
+	},
+	{
+		path: 'perfil',
+		component: PerfilComponent
+	},
+	{
+		path: 'publicacion/:id',
+		component: PublicacionComponent
+	}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
-export class RoutesModule { 
-  
-}
+export class RoutesModule {}
